9907dbc4c7246fa8900571e4e8bd238cae71b8a1
Gerry Baird <gerry.baird@uk.ibm.com>
2025-10-20 16:16:15 +0100

    Latest labs, removed obsolete labs

--- labs/ADK/wxo-life-agent/tools/life_quote.py
+++ labs/ADK/wxo-life-agent/tools/life_quote.py
@@ -3,15 +3,13 @@ import string
 from ibm_watsonx_orchestrate.agent_builder.tools import tool
 
 @tool(name="life_quote",
-      description="Calculates a coverage premium for life insurance when provided with amount of cover required, the age of the customer, and their involvement in extreme sports.")
-def life_quote(amount:int, age:int, ex_sports:bool):
+      description="Calculates a coverage premium for life insurance when provided with amount of cover required and the age of the customer.")
+def life_quote(amount:int, age:int):
 
     # the baseline cost for cover is 0.1% of the cover amount
     base_cost = amount * 0.001
     age_factor = (age / 1.5) * 0.1
     age_supplement = base_cost * age_factor
-    sport_factor = 0.35 if ex_sports else 0
-    sport_supplement = age_supplement * sport_factor
 
-    quote = base_cost + age_supplement + sport_supplement
+    quote = base_cost + age_supplement
     return quote
